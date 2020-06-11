923acce16a89fac866d1120c1957743e346a72ed
artnest <artnest789@gmail.com>
2020-06-11 18:02:28 +0300

    Initial Commit

---
+++ Wi-Fi Hotspot Connector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wi-Fi Hotspot Connector
+//
+//  Created by Artyom Nesterenko on 11/6/20.
+//  Copyright © 2020 artnest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
